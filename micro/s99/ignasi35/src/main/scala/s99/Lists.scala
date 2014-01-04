18656dfa7c7ff3e1bde410998fb77af3deae2edc
Ignasi Marimon-Clos <ignasi35@gmail.com>
2014-01-04 16:39:15 +0100

    refactorred production code: applied usage of generics

--- micro/s99/ignasi35/src/main/scala/s99/Lists.scala
+++ micro/s99/ignasi35/src/main/scala/s99/Lists.scala
@@ -2,11 +2,11 @@ package s99
 
 object Lists {
 
-  def last(in: List[Int]) : Int = in.reverse.head
+  def last[T](in: List[T]) : T = in.reverse.head
 
-  def penultimate(in:List[Int]) : Int = in.reverse.tail.head
+  def penultimate[T](in:List[T]) : T = in.reverse.tail.head
 
-  def nth(index: Int, xs:List[Int]) : Int = index match {
+  def nth[T](index: Int, xs:List[T]) : T = index match {
     case 0 => xs.head
     case _ => nth(index-1, xs.tail)
   } 
