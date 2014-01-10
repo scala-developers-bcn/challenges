1d4f05903d23711d2ff3c69c510e52fd005e31aa
Ignasi Marimon-Clos <ignasi35@gmail.com>
2014-01-10 20:16:50 +0100

    Solved P11

--- micro/s99/ignasi35/src/main/scala/s99/Lists.scala
+++ micro/s99/ignasi35/src/main/scala/s99/Lists.scala
@@ -2,54 +2,67 @@ package s99
 
 object Lists {
 
-  def last[T](xs: List[T]) : T = xs.reverse.head
+  def last[T](xs: List[T]): T = xs.reverse.head
 
-  def penultimate[T](xs:List[T]) : T = xs.reverse.tail.head
+  def penultimate[T](xs: List[T]): T = xs.reverse.tail.head
 
-  @scala.annotation.tailrec 
-  def nth[T](index: Int, xs:List[T]) : T = index match {
+  @scala.annotation.tailrec
+  def nth[T](index: Int, xs: List[T]): T = index match {
     case 0 => xs.head
-    case _ => nth(index-1, xs.tail)
-  } 
+    case _ => nth(index - 1, xs.tail)
+  }
 
-  def length[T](xs: List[T] ) : Int = xs.foldLeft(0)((acc,x) => acc+1)
+  def length[T](xs: List[T]): Int = xs.foldLeft(0)((acc, x) => acc + 1)
 
-  def reverse[T](xs: List[T] ) : List[T] = xs.foldLeft(List[T]())( (acc,x) => x :: acc  )
+  def reverse[T](xs: List[T]): List[T] = xs.foldLeft(List[T]())((acc, x) => x :: acc)
 
   // I suspect this can be tailrec but still havent wrapped my head around it...
-  def isPalindrome[T](xs: List[T]) : Boolean = {
+  def isPalindrome[T](xs: List[T]): Boolean = {
     xs match {
       case Nil => true
       case x :: Nil => true
-      case h :: tail => xs.head == last(xs) && isPalindrome(xs.tail.take(xs.tail.length-1))
+      case h :: tail => xs.head == last(xs) && isPalindrome(xs.tail.take(xs.tail.length - 1))
     }
   }
 
   // this solution I don't like,
-  def flatten(xss: List[Any]) : List[Any] = xss.foldLeft(List[Any]())((acc, x) => 
-     x match {
-        case l : List[_] => flatten(l).reverse ::: acc
-        case _ => x :: acc
-     }).reverse
-  
-  def compress[T](xs: List[T]) : List[T] = xs.foldLeft(List[T]())( 
-    (acc, x) => 
-       if (acc.isEmpty || x!=acc.head) x :: acc else acc
-   ).reverse
+  def flatten(xss: List[Any]): List[Any] = xss.foldLeft(List[Any]())((acc, x) =>
+    x match {
+      case l: List[_] => flatten(l).reverse ::: acc
+      case _ => x :: acc
+    }).reverse
 
-  def pack[T](xs: List[T]) : List[List[T]] = {
+  def compress[T](xs: List[T]): List[T] = xs.foldLeft(List[T]())(
+    (acc, x) =>
+      if (acc.isEmpty || x != acc.head) x :: acc else acc
+  ).reverse
+
+  def pack[T](xs: List[T]): List[List[T]] = {
     @scala.annotation.tailrec
-    def pack0(result: List[List[T]], acc: List[T], xs: List[T]):List[List[T]] = {
+    def pack0(result: List[List[T]], acc: List[T], xs: List[T]): List[List[T]] = {
       (acc, xs) match {
-       case (_, Nil)                       => (acc :: result).reverse
-       case (Nil, _)                       => pack0(result, List(xs.head), xs.tail)
-       case (_, h :: t) if (acc.head == h) => pack0(result, h :: acc, t)
-       case _                              => pack0(acc :: result, List(xs.head), xs.tail)
+        case (_, Nil) => (acc :: result).reverse
+        case (Nil, _) => pack0(result, List(xs.head), xs.tail)
+        case (_, h :: t) if (acc.head == h) => pack0(result, h :: acc, t)
+        case _ => pack0(acc :: result, List(xs.head), xs.tail)
       }
     }
     pack0(Nil, Nil, xs)
   }
 
-  def encode[T](xs: List[T]): List[(Int, T)] = pack(xs) map { l => (l.length, l.head) }
+  def encode[T](xs: List[T]): List[(Int, T)] = pack(xs) map {
+    l => (l.length, l.head)
+  }
+
+  def encodeModified[T](xs: List[T]): List[Any] = {
+    encode(xs) map {
+      case (count, key) =>
+        (count, key) match {
+          case (i, x) if i == 1 => x
+          case tuple => tuple
+        }
+    }
+  }
+
 
 }
--- micro/s99/ignasi35/src/test/scala/s99/Lists.scala
+++ micro/s99/ignasi35/src/test/scala/s99/Lists.scala
@@ -6,54 +6,115 @@ import org.scalatest.FlatSpec
 
 class ListsSpec extends FlatSpec with ShouldMatchers {
 
- import s99.Lists._
+  import s99.Lists._
 
- "A s99 problem set on lists" should "find the last element of a list" in {
-   last(List(1, 1, 2, 3, 5, 8)) should be(8)
- }
+  "A s99 problem set on lists" should "find the last element of a list" in {
+    last(List(1, 1, 2, 3, 5, 8)) should be(8)
+  }
 
- it should " Find the last but one element of a list." in {
-    penultimate(List(1, 1, 2, 3, 5, 8)) should be (5)
- }
+  it should " Find the last but one element of a list." in {
+    penultimate(List(1, 1, 2, 3, 5, 8)) should be(5)
+  }
 
- it should " Find the Kth element of a list.  (By convention, the first element in the list is element 0.)" in { 
+  it should " Find the Kth element of a list.  (By convention, the first element in the list is element 0.)" in {
     nth(2, List(1, 1, 2, 3, 5, 8)) should be(2)
- }
+  }
 
- it should " Find the number of elements of a list." in {
-  // #length seems to be a publicly available method and so even though I import Lists._ this other #length() is used instead causing a compilation error. Current solution I found was to add Lists. to the invotacion to ensure my Lists#length isbeing used.
-   Lists.length(List(1, 1, 2, 3, 5, 8)) should be(6)
- }
+  it should " Find the number of elements of a list." in {
+    // #length seems to be a publicly available method and so even though I import Lists._ this other #length() is used instead causing a compilation error. Current solution I found was to add Lists. to the invotacion to ensure my Lists#length isbeing used.
+    Lists.length(List(1, 1, 2, 3, 5, 8)) should be(6)
+  }
 
- it should "Reverse a list." in {
-   reverse(List(1, 1, 2, 3, 5, 8)) should be(List(8, 5, 3, 2, 1, 1))
- }
+  it should "Reverse a list." in {
+    reverse(List(1, 1, 2, 3, 5, 8)) should be(List(8, 5, 3, 2, 1, 1))
+  }
 
- it should "Find out whether a list is a palindrome." in {
-   isPalindrome(List(1, 2, 3, 2, 1)) should be (true)
- }
+  it should "Find out whether a list is a palindrome." in {
+    isPalindrome(List(1, 2, 3, 2, 1)) should be(true)
+  }
 
- it should "Not find a palindrome when there's none" in {
-   isPalindrome(List(1, 2, 3, 3, 1)) should be (false)
- }
+  it should "Not find a palindrome when there's none" in {
+    isPalindrome(List(1, 2, 3, 3, 1)) should be(false)
+  }
 
- it should "Flatten a nested list structure." in {
-   flatten(List(List(1, 1), 2, List(3, List(5, 8)))) should be (List(1, 1, 2, 3, 5, 8))
- }
+  it should "Flatten a nested list structure." in {
+    flatten(List(List(1, 1), 2, List(3, List(5, 8)))) should be(List(1, 1, 2, 3, 5, 8))
+  }
 
- it should "Eliminate consecutive duplicates of list elements. If a list contains repeated elements they should be replaced with a single copy of the element. The order of the elements should not be changed." in {
-    compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) should be( List('a, 'b, 'c, 'a, 'd, 'e) )
- }
+  it should "Eliminate consecutive duplicates of list elements. If a list contains repeated elements they should be replaced with a single copy of the element. The order of the elements should not be changed." in {
+    compress(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) should be(List('a, 'b, 'c, 'a, 'd, 'e))
+  }
 
- it should "Pack consecutive duplicates of list elements into sublists. If a list contains repeated elements they should be placed in separate sublists." in {
-   pack(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) should be(
-     List(List('a, 'a, 'a, 'a), List('b), List('c, 'c), List('a, 'a), List('d), List('e, 'e, 'e, 'e))
+  it should "Pack consecutive duplicates of list elements into sublists. If a list contains repeated elements they should be placed in separate sublists." in {
+    pack(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) should be(
+      List(List('a, 'a, 'a, 'a), List('b), List('c, 'c), List('a, 'a), List('d), List('e, 'e, 'e, 'e))
     )
- }
+  }
 
- it should "Run-length encoding of a list.  Use the result of problem P09 to implement the so-called run-length encoding data compression method. Consecutive duplicates of elements are encoded as tuples (N, E) where N is the number of duplicates of the element E." in {
+  it should "Run-length encoding of a list.  Use the result of problem P09 to implement the so-called run-length encoding data compression method. Consecutive duplicates of elements are encoded as tuples (N, E) where N is the number of duplicates of the element E." in {
     encode(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) should be(
-      List((4,'a), (1,'b), (2,'c), (2,'a), (1,'d), (4,'e)))
- }
+      List((4, 'a), (1, 'b), (2, 'c), (2, 'a), (1, 'd), (4, 'e)))
+  }
+
+  it should "Modified run-length encoding. Modify the result of problem P10 in such a way that if an element has no duplicates it is simply copied into the result list. Only elements with duplicates are transferred as (N, E) terms." in {
+    encodeModified(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)) should be(
+      List((4, 'a), 'b, (2, 'c), (2, 'a), 'd, (4, 'e)))
+  }
+
+  /*
+
+
+P12 (**) Decode a run-length encoded list.
+Given a run-length code list generated as specified in problem P10, construct its uncompressed version.
+Example:
+
+scala> decode(List((4, 'a), (1, 'b), (2, 'c), (2, 'a), (1, 'd), (4, 'e)))
+res0: List[Symbol] = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e)
+P13 (**) Run-length encoding of a list (direct solution).
+Implement the so-called run-length encoding data compression method directly. I.e. don't use other methods you've written (like P09's pack); do all the work directly.
+Example:
+
+scala> encodeDirect(List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e))
+res0: List[(Int, Symbol)] = List((4,'a), (1,'b), (2,'c), (2,'a), (1,'d), (4,'e))
+P14 (*) Duplicate the elements of a list.
+Example:
+scala> duplicate(List('a, 'b, 'c, 'c, 'd))
+res0: List[Symbol] = List('a, 'a, 'b, 'b, 'c, 'c, 'c, 'c, 'd, 'd)
+P15 (**) Duplicate the elements of a list a given number of times.
+Example:
+scala> duplicateN(3, List('a, 'b, 'c, 'c, 'd))
+res0: List[Symbol] = List('a, 'a, 'a, 'b, 'b, 'b, 'c, 'c, 'c, 'c, 'c, 'c, 'd, 'd, 'd)
+P16 (**) Drop every Nth element from a list.
+Example:
+scala> drop(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+res0: List[Symbol] = List('a, 'b, 'd, 'e, 'g, 'h, 'j, 'k)
+P17 (*) Split a list into two parts.
+The length of the first part is given. Use a Tuple for your result.
+Example:
+
+scala> split(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+res0: (List[Symbol], List[Symbol]) = (List('a, 'b, 'c),List('d, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+P18 (**) Extract a slice from a list.
+Given two indices, I and K, the slice is the list containing the elements from and including the Ith element up to but not including the Kth element of the original list. Start counting the elements with 0.
+Example:
+
+scala> slice(3, 7, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+res0: List[Symbol] = List('d, 'e, 'f, 'g)
+P19 (**) Rotate a list N places to the left.
+Examples:
+scala> rotate(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+res0: List[Symbol] = List('d, 'e, 'f, 'g, 'h, 'i, 'j, 'k, 'a, 'b, 'c)
+
+scala> rotate(-2, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+res1: List[Symbol] = List('j, 'k, 'a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i)
+P20 (*) Remove the Kth element from a list.
+Return the list and the removed element in a Tuple. Elements are numbered from 0.
+Example:
+
+scala> removeAt(1, List('a, 'b, 'c, 'd))
+res0: (List[Symbol], Symbol) = (List('a, 'c, 'd),'b)
+
+   */
+
 
 }
