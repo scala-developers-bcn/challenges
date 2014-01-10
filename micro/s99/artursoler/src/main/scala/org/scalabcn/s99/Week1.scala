c2cd84a41d2bd9ec05bfa05964e378a38d936659
Artur Soler <artur.soler@gmail.com>
2014-01-10 21:52:48 +0100

    Refactor to a single solution per problem.

--- micro/s99/artursoler/src/main/scala/org/scalabcn/s99/Week1.scala
+++ micro/s99/artursoler/src/main/scala/org/scalabcn/s99/Week1.scala
@@ -3,94 +3,23 @@ package org.scalabcn.s99
 import scala.annotation.tailrec
 
 object Week1 {
-  object Last {
-    def trivial[T](xs: List[T]): T = xs.last
+  def last[T](xs: List[T]) = (xs.head /: xs)((discard: T, keep: T) => keep)
 
-    def reversing[T](xs: List[T]): T = xs.reverse.head
-
-    @tailrec
-    def recursive[T](xs: List[T]): T = xs match {
-      case x :: Nil => x
-      case x :: xs => recursive(xs)
-      case Nil => throw new NoSuchElementException
-    }
-
-    def fold[T](xs: List[T]): T = (xs.head /: xs)((a: T, b: T) => b)
-
-    def funcs[T]: List[List[T] => T] = List(trivial[T], reversing[T], recursive[T], fold[T])
+  // xs.init.last being the obvious
+  @tailrec
+  def penultimate[T](xs: List[T]): T = xs match {
+    case x :: y :: Nil => x
+    case x :: Nil => throw new NoSuchElementException
+    case Nil => throw new NoSuchElementException
+    case x :: xs => penultimate(xs)
   }
 
-  object Penultimate {
-    def trivial[T](xs: List[T]): T = xs.init.last
+  @tailrec
+  def nth[T](pos: Int, xs: List[T]): T =
+    if (pos == 0) xs.head
+    else nth(pos-1, xs.tail)
 
-    def reversing[T](xs: List[T]): T = xs.reverse.tail.head
+  def length[T](xs: List[T]) = (xs :\ 0)((elem, acc) => acc+1)
 
-    def sliding[T](xs: List[T]): T = xs.sliding(2).toList.last.head
-
-    @tailrec
-    def recursive[T](xs: List[T]): T = xs match {
-      case x :: y :: Nil => x
-      case x :: Nil => throw new NoSuchElementException
-      case Nil => throw new NoSuchElementException
-      case x :: xs => recursive(xs)
-    }
-
-    def funcs[T]: List[List[T] => T] = List(trivial[T], reversing[T], sliding[T], recursive[T])
+  def reverse[T](xs: List[T]) = (List[T]() /: xs)((acc, x) => x :: acc)
   }
-
-  object Nth {
-    def trivial[T](pos: Int, xs: List[T]): T = xs(pos)
-
-    @tailrec
-    def recursive[T](pos: Int, xs: List[T]): T =
-      if (pos == 0)
-        xs.head
-      else
-        recursive(pos-1, xs.tail)
-
-    def funcs[T]: List[(Int, List[T])=> T] = List(trivial[T], recursive[T])
-  }
-
-  object Length {
-    def trivial[T](xs: List[T]): Int = xs.length
-
-    def recursive[T](xs: List[T]): Int = xs match {
-      case Nil => 0
-      case x :: xs => 1 + recursive(xs)
-    }
-
-    def recursiveTailrec[T](xs: List[T]) = {
-      @tailrec
-      def inner[T](xs: List[T], acc: Int): Int = xs match {
-        case Nil => acc
-        case x :: xs => inner(xs, acc+1)
-      }
-      inner(xs, 0)
-    }
-
-    def fold[T](xs: List[T]) = (xs :\ 0)((elem, acc) => acc+1)
-
-    def funcs[T]: List[List[T] => Int] = List(trivial[T], recursive[T], recursiveTailrec[T], fold[T])
-  }
-
-  object Reverse {
-    def trivial[T](xs: List[T]) = xs.reverse
-
-    def recursive[T](xs: List[T]): List[T] = xs match {
-      case Nil => Nil
-      case x :: xs => recursive(xs) ::: List(x)
-    }
-
-    def recursiveTailrec[T](xs: List[T]) = {
-      def inner[T](xs: List[T], acc: List[T]): List[T] = xs match {
-        case Nil => acc
-        case x :: xs => inner(xs, x :: acc)
-      }
-      inner(xs, List[T]())
-    }
-
-    def fold[T](xs: List[T]) = (List[T]() /: xs)((acc, x) => x :: acc)
-
-    def funcs[T]: List[List[T] => List[T]] = List(trivial[T], recursive[T], recursiveTailrec[T], fold[T])
-  }
-}
--- micro/s99/artursoler/src/test/scala/org/scalabcn/s99/Week1FlatSpec.scala
+++ micro/s99/artursoler/src/test/scala/org/scalabcn/s99/Week1FlatSpec.scala
@@ -5,56 +5,36 @@ import org.scalatest.prop.PropertyChecks
 import org.scalabcn.s99.Week1._
 
 class Week1FlatSpec extends FlatSpec with Matchers with PropertyChecks {
-  def p01[T](xs: List[T], result: T) =
-    for(func <- Last.funcs[T])
-      func(xs) should equal (result)
-
-  "Last (solution 1)" should "return last element of list" in {
-    forAll { (init: List[Int], last: Int) =>
-      val complete = (last :: init).reverse
-      p01(complete, last)
+  "Last (P01)" should "return last element of list" in {
+    forAll { (init: List[Int], end: Int) =>
+      val complete = init ::: List(end)
+      last(complete) should be (end)
     }
   }
 
-  def p02[T](xs: List[T], result: T) =
-    for(func <- Penultimate.funcs[T])
-      func(xs) should equal (result)
-
-  "Penultimate (solution 2)" should "return the last but one element" in {
-    forAll { (init: List[Int], penultimate: Int, last: Int) =>
-      val complete = (last :: penultimate :: init).reverse
-      p02(complete, penultimate)
+  "Penultimate (P02)" should "return the last but one element" in {
+    forAll { (init: List[Int], target: Int, last: Int) =>
+      val complete = init ::: List(target) ::: List(last)
+      penultimate(complete) should be (target)
     }
   }
 
-  def p03[T](pos: Int, xs: List[T], result: T) =
-    for(func <- Nth.funcs[T])
-      func(pos, xs) should equal (result)
-
-  "Nth (solution 3)" should "return the element at position n" in {
+  "Nth (P03)" should "return the element at position n" in {
     forAll { (first: List[Int], second: List[Int], between: Int) =>
       val complete = first ::: (between :: second)
-      p03(first.length, complete, between)
+      nth(first.length, complete) should be (between)
     }
   }
 
-  def p04[T](xs: List[T], result: T) =
-    for(func <- Length.funcs[T])
-      func(xs) should equal (result)
-
-  "Length (solution 4)" should "return the length of the list" in {
+  "Length (P04)" should "return the length of the list" in {
     forAll { (list: List[Int]) =>
-      p04(list, list.length)
+      Week1.length(list) should be (list.length)
     }
   }
 
-  def p05[T](xs: List[T], result: T) =
-    for(func <- Reverse.funcs[T])
-      func(xs) should equal (result)
-
-  "Reverse (solution 5)" should "return the original list reversed" in {
+  "Reverse (P05)" should "return the original list reversed" in {
     forAll { (list: List[Int]) =>
-      p05(list, list.reverse)
+      reverse(list) should be (list.reverse)
     }
   }
 
