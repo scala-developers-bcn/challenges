1ba3e22b0649afe29a3ddea37efb480074ffafeb
Pablo Díaz <padilo@gmail.com>
2014-01-19 23:12:57 +0100

    Correction on delete and put services. Updated postman export

--- monthly/2014-01/padilo/src/main/scala/net/pdiaz/flyapp/app/AppMappings.scala
+++ monthly/2014-01/padilo/src/main/scala/net/pdiaz/flyapp/app/AppMappings.scala
@@ -10,7 +10,7 @@ class App extends unfiltered.filter.Plan {
 
   def intent = {
     case GET(Path(Seg(Nil))) => {
-      Ok ~> ResponseString("Welcome\n\nThis is under construction no services are implemented but are published :)\n\nDownload chrome postman at https://chrome.google.com/webstore/detail/postman-rest-client-packa/fhbjgbiflinjbdggehcddcbncdddomop\n\nAnd import https://www.getpostman.com/collections/bca9b1540fbc3a5e52be")
+      Ok ~> ResponseString("Welcome\n\nThis is under construction no services are implemented but are published :)\n\nDownload chrome postman at https://chrome.google.com/webstore/detail/postman-rest-client-packa/fhbjgbiflinjbdggehcddcbncdddomop\n\nAnd import https://www.getpostman.com/collections/785dcb9f5a081a1caea4")
     }
     case POST(Path(Seg("flight" :: Nil))) => {
       Ok ~> ResponseString("POST a flight (id, arrival, departure, from, to, status)")
@@ -21,8 +21,8 @@ class App extends unfiltered.filter.Plan {
     case PUT(Path(Seg("flight" :: id :: Nil))) => {
       Ok ~> ResponseString(s"PUT an updated status to a flight (i.e: DELAYED, CANCELLED, Gate 42) [id: ${id}]")
     }
-    case DELETE(Path(Seg("flight" :: Nil))) => {
-      Ok ~> ResponseString("DELETE a flight (maybe) [id: ${id}]")
+    case DELETE(Path(Seg("flight" :: id :: Nil))) => {
+      Ok ~> ResponseString(s"DELETE a flight (maybe) [id: ${id}]")
     }
     case _ => {
       NotFound ~> ResponseString("Nice try, but this is not found ;-)")
