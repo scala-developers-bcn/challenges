3f43f37d7c9d7a4559558d33aa6bd730d47ec78c
Guillem Mercadal <guillem.mercadal@gmail.com>
2014-01-19 23:09:02 +0100

    Clean code

--- monthly/2014-01/akustik/flyApp/app/controllers/Flights.scala
+++ monthly/2014-01/akustik/flyApp/app/controllers/Flights.scala
@@ -8,21 +8,10 @@ import views._
 
 import play.api.libs.json._
 
-object Flights extends Controller {
+case class Flight(id: String, from: String, to: String,
+  arrival: Long, departure: Long, status: String)
 
-  case class Flight(id: String, from: String, to: String,
-    arrival: Long, departure: Long, status: String)
-
-  implicit val flightReads = Json.reads[Flight]
-  implicit val flightWrites = Json.writes[Flight]
-
-  implicit var flightDB = scala.collection.mutable.Map[String, Flight]()
-
-  val responseSuccess = Ok(Json.obj("result" -> "success"))
-  val responseFailure = Ok(Json.obj("result" -> "failure"))
-  val responseJsonError = BadRequest("json parse error")
-  val responseFlights = (flights: Iterable[Flight]) => Ok(Json.toJson(flights))
-
+trait Logging {
   def log(msg: String) = {
     println(s"LOG: $msg")
   }
@@ -30,12 +19,71 @@ object Flights extends Controller {
   def error(msg: String) = {
     println(s"ERROR: $msg")
   }
+}
+
+trait FlightDB extends Logging {
+  def insert(f: Flight)
+  def flightsTo(id: String, from: Long, to: Long): Iterable[Flight]
+  def flightsFrom(id: String, from: Long, to: Long): Iterable[Flight]
+  def updateStatus(id: String, status: String)
+  def delete(id: String)
+}
+
+class MemoryFlightDB extends FlightDB {
+  var flightDB = scala.collection.mutable.Map[String, Flight]()
+
+  private val timestampInRange = (t: Long, from: Long, to: Long) => (from, to) match {
+    case (0, 0) => true
+    case (0, b) => t <= b
+    case (a, 0) => a <= t
+    case (a, b) => a <= t && t <= b
+  }
 
   def insert(f: Flight) = {
     flightDB.put(f.id, f).foreach(old => log("Removed previous flight: " + old))
   }
 
-  def validateOrFailure[T](validate: (JsValue) => JsResult[T], operate: (T) => Unit) = Action(parse.json) {
+  private def filter(sel: (Flight) => String, ts: (Flight) => Long)(id: String, from: Long, to: Long) = {
+    flightDB.filter {
+      case (flightId, flight) => sel(flight) == id && timestampInRange(ts(flight), from, to)
+    }.values
+  }
+
+  def flightsTo(id: String, from: Long, to: Long): Iterable[Flight] = {
+    filter(f => f.to, f => f.arrival)(id, from, to)
+  }
+
+  def flightsFrom(id: String, from: Long, to: Long): Iterable[Flight] = {
+    filter(f => f.from, f => f.arrival)(id, from, to)
+  }
+
+  def updateStatus(id: String, status: String) = {
+    flightDB.get(id) match {
+      case Some(f) => {
+        insert(Flight(f.id, f.from, f.to, f.arrival, f.departure, status))
+      }
+      case None => throw new IllegalStateException(s"Flight ${id} not found")
+    }
+  }
+
+  def delete(id: String) = {
+    flightDB -= id
+  }
+}
+
+object Flights extends Controller with Logging {
+
+  implicit val flightReads = Json.reads[Flight]
+  implicit val flightWrites = Json.writes[Flight]
+  implicit val db = new MemoryFlightDB
+
+  private val responseSuccess = Ok(Json.obj("result" -> "success"))
+  private val responseFailure = Ok(Json.obj("result" -> "failure"))
+  private val responseJsonError = BadRequest("json parse error")
+  private val responseFlights = (flights: Iterable[Flight]) => Ok(Json.toJson(flights))
+
+  private def validateAndExecute[T](validate: (JsValue) => JsResult[T],
+    operate: (T) => Unit) = Action(parse.json) {
     request =>
       {
         validate(request.body).map {
@@ -62,43 +110,47 @@ object Flights extends Controller {
       }
   }
 
-  def create() = validateOrFailure(_.validate[Flight], insert)
-
-  val timestampInRange = (t: Long, from: Long, to: Long) => (from, to) match {
-    case (0, 0) => true
-    case (0, b) => t <= b
-    case (a, 0) => a <= t
-    case (a, b) => a <= t && t <= b
-  }
-
-  def filter(sel: (Flight) => String, ts: (Flight) => Long)(id: String, from: Long, to: Long) = {
-    flightDB.filter {
-      case (flightId, flight) => sel(flight) == id && timestampInRange(ts(flight), from, to)
-    }.values
-  }
-
-  val flightsTo = filter(f => f.to, f => f.arrival)_
-  val flightsFrom = filter(f => f.from, f => f.arrival)_
-
-  def to(id: String, from: Long, to: Long) = Action {
-    responseFlights(flightsTo(id, from, to))
-  }
-
-  def from(id: String, from: Long, to: Long) = Action {
-    responseFlights(flightsFrom(id, from, to))
-  }
-
-  def delete(id: String) = Action {
-    flightDB -= id
-    responseSuccess
-  }
-
-  def updateStatus(id: String) = validateOrFailure(_.validate[String], (status: String) => {
-    flightDB.get(id) match {
-      case Some(f) => {
-        insert(Flight(f.id, f.from, f.to, f.arrival, f.departure, status))
+  private def list(block: => Iterable[Flight]) = Action {
+    try {
+      responseFlights(block)
+    } catch {
+      case e: Throwable => {
+        error(e.toString())
+        responseFailure
       }
-      case None => throw new IllegalStateException(s"Flight ${id} not found")
     }
+  }
+
+  private def execute(block: => Unit) = Action {
+    try {
+      block
+      responseSuccess
+    } catch {
+      case e: Throwable => {
+        error(e.toString())
+        responseFailure
+      }
+    }
+  }
+
+  def create() = {
+    validateAndExecute(_.validate[Flight], (f: Flight) => db.insert(f))
+  }
+
+  def to(id: String, from: Long, to: Long) = list {
+    db.flightsTo(id, from, to)
+  }
+
+  def from(id: String, from: Long, to: Long) = list {
+    db.flightsFrom(id, from, to)
+  }
+
+  def delete(id: String) = execute {
+    db.delete(id)
+  }
+
+  def updateStatus(id: String) = validateAndExecute(_.validate[String], (status: String) => {
+    db.updateStatus(id, status)
   })
+
 }
--- monthly/2014-01/akustik/flyApp/test/FlightsSpec.scala
+++ monthly/2014-01/akustik/flyApp/test/FlightsSpec.scala
@@ -1,15 +1,14 @@
 import org.specs2.mutable._
 import org.specs2.runner._
 import org.junit.runner._
-
 import play.api.test._
 import play.api.test.Helpers._
-
 import play.api.libs.json._
+
 
 @RunWith(classOf[JUnitRunner])
 class FlightsSpec extends Specification with FlightData {
-
+  
   "Flights" should {
 
     "send 404 on a bad request" in {
