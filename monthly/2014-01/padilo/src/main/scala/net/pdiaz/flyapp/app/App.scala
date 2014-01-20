fb1427642631dec2bb422be926c0d37455704495
Pablo Díaz <padilo@gmail.com>
2014-01-20 23:58:00 +0100

    Rebuild paths using Directives and implementation of memstorage with concurrency problems.

---
+++ monthly/2014-01/padilo/src/main/scala/net/pdiaz/flyapp/app/App.scala
@@ -0,0 +1,144 @@
+package net.pdiaz.flyapp.app
+
+import unfiltered.request._
+import unfiltered.response._
+import unfiltered.directives._
+import unfiltered.directives.Directives._
+import net.pdiaz.flyapp.stores.FlightMemStore
+import dispatch.classic._
+import net.pdiaz.flyapp.stores.{ Flight, FlightStore, DataAlreadyExists, StoreDone, DataNotFound, ModifyDone }
+import argonaut.integrate.unfiltered._
+import argonaut._
+import Argonaut._
+import scalaz._
+import Scalaz._
+import com.github.nscala_time.time.Imports._
+
+//** unfiltered plan */
+class FlightPlan(flightStore: FlightStore = FlightMemStore) extends unfiltered.filter.Plan {
+
+  // it's simple to define your own directives
+  def ensureContentType(tpe: String) =
+    when { case RequestContentType(`tpe`) => } orElse UnsupportedMediaType
+
+  def ensureJSONContentType = ensureContentType("application/json")
+
+  def intent = Path.Intent {
+    case Seg(List("flights")) => {
+      def get = for {
+        _ <- GET
+        _ <- Accepts.Json
+        toOption <- data.as.Option[String] named "to"
+        fromOption <- data.as.Option[String] named "from"
+      } yield {
+        Ok ~> JsonResponse(flightStore.listFilter(toOption, fromOption))
+      }
+
+      get
+    }
+    case Seg(List("flights", key)) => {
+      def put = for {
+        _ <- PUT
+        _ <- Accepts.Json
+        _ <- ensureJSONContentType
+        r <- request[Any]
+      } yield {
+        Body.string(r).decodeOption[Flight] match {
+          case Some(flight) => {
+            flightStore.update(key, flight) match {
+              case DataNotFound => {
+                NotFound
+              }
+              case ModifyDone => {
+                NoContent
+              }
+            }
+          }
+          case None => {
+            BadRequest
+          }
+        }
+      }
+
+      def post = for {
+        _ <- POST
+        _ <- Accepts.Json
+        _ <- ensureJSONContentType
+        r <- request[Any]
+      } yield {
+        Body.string(r).decodeOption[Flight] match {
+          case Some(flight) => {
+            flightStore.store(key, flight) match {
+              case DataAlreadyExists => {
+                Conflict
+              }
+              case StoreDone => {
+                Created
+              }
+            }
+          }
+          case None => {
+            BadRequest
+          }
+        }
+
+      }
+
+      def delete = for {
+        _ <- DELETE
+        _ <- Accepts.Json
+      } yield {
+        flightStore.delete(key) match {
+          case DataNotFound => {
+            NotFound
+          }
+          case ModifyDone => {
+            NoContent
+          }
+        }
+      }
+
+      put | delete | post
+    }
+  }
+}
+
+class HomePagePlan extends unfiltered.filter.Plan {
+  def intent = {
+    case Path("/") => {
+      Ok ~> Html5(
+        <html>
+          <head><title>Pablo monthly Scala</title></head>
+          <body>
+    		  <p>
+    		  	Welcome
+    		  </p>
+    		  <p>
+    		  	This is under construction but nearly finish, I need to investigate a bit how to test this :)
+    		  	To try this API, download chrome postman <a href="https://chrome.google.com/webstore/detail/postman-rest-client-packa/fhbjgbiflinjbdggehcddcbncdddomop">HERE</a>
+    		  </p>
+    		  <p>
+    		  	And import <a href="https://www.getpostman.com/collections/cb088452d39f131e4b7f">this</a>
+    		  </p>
+    		  <p>
+    		  	If you want to play by yourself here you have the /flights resource at http://127.0.0.1/flights with some data
+    		  </p>
+    		  <ul>
+    		  	<li>POST /flights/[id]: To create a new resource path parameter id</li>
+    		  	<li>PUT /flights/[id]: To change an already existing resource with path parameter id</li>
+    		  	<li>DELETE /flights/[id]: To delete an already existing resource with path parameter id</li>
+    		  	<li>GET /flights: To retrieve all the flights within time, also accepts to do queries usign URL params (to,from) to filter the data</li>
+    		  </ul>
+    		  <hr />
+    		  Pablo D&iacute;az
+          </body>
+        </html>)
+    }
+  }
+}
+
+object Server {
+  def main(args: Array[String]) {
+    unfiltered.jetty.Http(8080).plan(new HomePagePlan).plan(new FlightPlan).run()
+  }
+}
---
+++ monthly/2014-01/padilo/src/main/scala/net/pdiaz/flyapp/stores/FlightStore.scala
@@ -0,0 +1,100 @@
+package net.pdiaz.flyapp.stores
+
+import java.util.Date
+import scala.collection._
+import com.github.nscala_time.time.Imports._
+
+// Results
+trait StoreResult
+case object StoreDone extends StoreResult
+case object DataAlreadyExists extends StoreResult
+
+trait ModifyResult
+case object ModifyDone extends ModifyResult
+case object DataNotFound extends ModifyResult
+
+// Flight Status
+object FlightStatus extends Enumeration {
+  type FlightStatus = Value
+  val Delayed, Cancelled, OnTime = Value
+}
+
+case class Flight(departure: DateTime, arrival: DateTime, from: String, to: String, status: FlightStatus.Value)
+
+trait FlightStore {
+  def listFilter(toFilter: Option[String], fromFilter: Option[String]): List[(String,Flight)]
+  def list(): List[(String,Flight)]
+  def store(key:String, flight: Flight): StoreResult
+  def update(key: String, flight: Flight): ModifyResult
+  def delete(key: String): ModifyResult
+}
+
+object FlightMemStore extends FlightStore {
+  private val dataMap = new mutable.HashMap[String, Flight]
+
+  dataMap("1") = Flight(DateTime.now.hour(9).minute(5).second(0), DateTime.now.hour(10).minute(0).second(0), "Barcelona", "Madrid", FlightStatus.OnTime)
+  dataMap("2") = Flight(DateTime.now.hour(10).minute(20).second(0), DateTime.now.hour(11).minute(15).second(0), "Madrid", "Barcelona", FlightStatus.Delayed)
+  dataMap("3") = Flight(DateTime.now.hour(16).minute(10).second(0), DateTime.now.hour(17).minute(0).second(0), "Zaragoza", "Coruña", FlightStatus.Cancelled)
+  dataMap("4") = Flight(DateTime.now.hour(0).minute(20).second(0), DateTime.now.hour(12).minute(10).second(0), "Coruña", "Madrid", FlightStatus.OnTime)
+  dataMap("5") = Flight(DateTime.now.hour(12).minute(30).second(0), DateTime.now.hour(13).minute(45).second(0), "Barcelona", "Zaragoza", FlightStatus.OnTime)
+
+  def store(key:String, flight: Flight): StoreResult =
+    if (dataMap.contains(key)) {
+      DataAlreadyExists
+    } else {
+      dataMap.put(key, flight)
+      StoreDone
+    }
+
+  def get(key: String): Option[Flight] = {
+    dataMap.get(key)
+  }
+
+  private def ifExists(key: String)(doThings: => Unit) = {
+    if (dataMap.contains(key)) {
+      doThings
+      ModifyDone
+    } else DataNotFound
+  }
+
+  def update(key: String, flight: Flight): ModifyResult = {
+    ifExists(key) {
+      dataMap.update(key, flight)
+    }
+  }
+  def delete(key: String): ModifyResult = {
+    ifExists(key) {
+      dataMap.remove(key)
+    }
+  }
+
+  def list(): List[(String,Flight)] = {
+    listFilter(None, None)
+  }
+
+  def listFilter(toFilter: Option[String], fromFilter: Option[String]): List[(String,Flight)] = {
+    def filterFunc(option: Option[String]) = option match {
+      case Some(att) => {
+        (flightAtt: String) =>
+          {
+            att == flightAtt
+          }
+      }
+      case None => {
+        (_: String) => true
+      }
+    }
+
+    val toFilterFunc = filterFunc(toFilter)
+    val fromFilterFunc = filterFunc(fromFilter)
+
+    def iterable = for {
+      entry <- dataMap
+      flight = entry._2
+      key = entry._1
+      if(toFilterFunc(flight.to) && fromFilterFunc(flight.from) && DateTime.now <= flight.departure)
+    } yield entry
+
+    iterable.toList
+  }
+}
