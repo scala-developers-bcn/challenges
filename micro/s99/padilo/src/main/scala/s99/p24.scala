2a756c83f29ab3efa46373a7e345b8e4373b59dd
Pablo Díaz <padilo@gmail.com>
2014-01-24 20:11:37 +0100

    The already done p21 to p25

---
+++ micro/s99/padilo/src/main/scala/s99/p24.scala
@@ -0,0 +1,23 @@
+package s99
+
+import scala.util.Random
+/*
+ * Lotto: Draw N different random numbers from the set 1..M.
+ * Example:
+ * scala> lotto(6, 49)
+ * res0: List[Int] = List(23, 1, 17, 33, 21, 37)
+ */
+object p24 {
+  def main(args: Array[String]) {
+    println(lotto(6, 49))
+    println(lotto2(6, 49))
+  }
+  // My solution, I win this time :)
+  def lotto(n: Int, m: Int) = {
+    List.fill(n)(Random.nextInt(m)+1)    
+  }
+  
+  // Phil's solution
+  def lotto2(count: Int, max: Int): List[Int] = 
+    p23.randomSelect(count, List.range(1, max + 1))
+}
---
+++ micro/s99/padilo/src/main/scala/s99/p25.scala
@@ -0,0 +1,23 @@
+package s99
+
+import scala.collection.mutable.ArraySeq
+
+/*
+ * Generate a random permutation of the elements of a list.
+ * Hint: Use the solution of problem P23.
+ * Example:
+ * 
+ * scala> randomPermute(List('a, 'b, 'c, 'd, 'e, 'f))
+ * res0: List[Symbol] = List('b, 'a, 'd, 'c, 'e, 'f)
+ */
+object p25 {
+  def main(args: Array[String]) = {
+	  println(randomPermute(List('a, 'b, 'c, 'd, 'e, 'f)))
+  }
+  
+  // Using P23
+  def randomPermute[T](l:List[T]):List[T] = {
+    p23.randomSelect(l.length, l)
+  }
+  
+}
