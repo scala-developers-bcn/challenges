8fd0ba32cd70312ea242715eb40b3828a9ef4081
Ignasi Marimon-Clos <ignasi35@gmail.com>
2014-01-25 18:25:13 +0100

    Tests GET /flights return the default list of flights in JSON format

--- monthly/2014-01/ignasi35/play/january-challenge/app/controllers/Application.scala
+++ monthly/2014-01/ignasi35/play/january-challenge/app/controllers/Application.scala
@@ -4,36 +4,30 @@ import play.api._
 import play.api.mvc._
 import play.api.libs.json._
 
-object FlightDomain {
-  type CityCode = String
+case class Flight(status: String, origin: String, destiny: String, gate: String)
 
-  case class Flight // (status: FlightStatus , origin: CityCode , destiny: CityCode,
-  (status: String, origin: String, destiny: String,
-    gate: String)
+object Protocol {
+  implicit val fmtFlight = Json.format[Flight]
+}
 
-  class FlightStatus extends Enumeration {
-    val SCHEDULED, DELAYED, CANCELLED, BOARDING, ON_AIR, LANDED = Value
-  }
+trait FlightRepository {
+  def loadAll(): List[Flight]
+}
 
-  object FlightStatus {
-    def unapply(fs: FlightStatus) = fs.toString
-  }
-
-  object Protocol {
-    //implicit val fmtCityCode = Json.format[CityCode]
-    //implicit val fmtFlightStatus = Json.format[FlightStatus]
-    implicit val fmtFlight = Json.format[Flight]
-  }
+object InMemFlightsRepository extends FlightRepository {
+  private val flights = List(Flight("ON_TIME", "BOS", "CHG", "19B"))
+  def loadAll(): List[Flight] = flights
 }
 
 object Application extends Controller {
 
-  import FlightDomain._
-  import FlightDomain.Protocol._
+  import Protocol._
 
   def addFlight = Action { NotFound }
 
-  def getFlights = Action { Ok(Json.toJson(List())) }
+  def getFlights = Action {
+    Ok(Json.toJson(InMemFlightsRepository.loadAll))
+  }
 
   // TODO filter by date range
   def getFlightsFrom(cityCode: String) = Action { NotFound }
--- monthly/2014-01/ignasi35/play/january-challenge/test/controllers/ApplicationSpec.scala
+++ monthly/2014-01/ignasi35/play/january-challenge/test/controllers/ApplicationSpec.scala
@@ -5,22 +5,20 @@ import org.specs2.mutable._
 import play.api.test._
 import play.api.test.Helpers._
 
-/**
- * Add your spec here.
- * You can mock out a whole application including requests, plugins etc.
- * For more information, consult the wiki.
- */
 class ApplicationSpec extends Specification {
 
   "Application" should {
 
     "return 200 on flights/" in {
       running(FakeApplication()) {
-        val home = route(FakeRequest(GET, "/flights")).get
-
-        status(home) must equalTo(OK)
-        contentType(home) must beSome.which(_ == "application/json")
+        val flights = route(FakeRequest(GET, "/flights")).get
+        status(flights) must equalTo(OK)
+        contentType(flights) must beSome.which(_ == "application/json")
+        contentAsString(flights) must equalTo(
+          """[{"status":"ON_TIME","origin":"BOS","destiny":"CHG","gate":"19B"}]""")
       }
     }
+
   }
+
 }
