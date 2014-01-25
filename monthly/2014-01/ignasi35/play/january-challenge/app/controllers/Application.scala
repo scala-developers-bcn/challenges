d92460a21bb677bacac107dd5057e3e43c019139
Ignasi Marimon-Clos <ignasi35@gmail.com>
2014-01-25 18:10:53 +0100

    Skeleton

--- monthly/2014-01/ignasi35/play/january-challenge/app/controllers/Application.scala
+++ monthly/2014-01/ignasi35/play/january-challenge/app/controllers/Application.scala
@@ -2,11 +2,48 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.libs.json._
+
+object FlightDomain {
+ type CityCode = String
+
+ case class Flight 
+// (status: FlightStatus , origin: CityCode , destiny: CityCode,
+(status: String , origin: String , destiny: String,
+   gate: String)
+
+ class FlightStatus extends Enumeration {
+   val SCHEDULED , DELAYED, CANCELLED, BOARDING, ON_AIR, LANDED = Value
+ }
+
+ object FlightStatus {
+  def unapply(fs:FlightStatus) = fs.toString
+ }
+
+ object Protocol {
+   //implicit val fmtCityCode = Json.format[CityCode]
+   //implicit val fmtFlightStatus = Json.format[FlightStatus]
+   implicit val fmtFlight = Json.format[Flight]
+ }
+}
 
 object Application extends Controller {
-  
-  def index = Action {
-    Ok("") //:Wviews.html.index("Your new application is ready."))
-  }
+
+ import FlightDomain._
+ import FlightDomain.Protocol._
+ 
+ def addFlight = Action{ NotFound }
+
+ def getFlights = Action { NotFound }
+
+ // TODO filter by date range
+ def getFlightsFrom(cityCode: String) = Action { NotFound }
+
+ // TODO filter by date range
+ def getFlightsTo(cityCode: String) = Action  { NotFound }
+
+ def updateFlight = Action { NotFound }
+
+ // def delete
   
 }
