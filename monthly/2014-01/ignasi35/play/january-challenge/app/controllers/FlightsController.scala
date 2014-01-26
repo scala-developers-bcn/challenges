83783602dab52d38ae2518a820f1bb6e7250dfd9
Ignasi Marimon-Clos <ignasi35@gmail.com>
2014-01-26 22:35:21 +0100

    [refactor] Simplified DI. Still uses object controller but allows DI and so testing can inject FakeImpl. Also, got rid of Global object. Also, testing is not loading a FakeApplication which is heavy like hell

--- monthly/2014-01/ignasi35/play/january-challenge/app/controllers/FlightsController.scala
+++ monthly/2014-01/ignasi35/play/january-challenge/app/controllers/FlightsController.scala
@@ -4,6 +4,7 @@ import play.api.libs.json._
 import play.api.mvc._
 import repositories.FlightsRepository
 import views.html.defaultpages.badRequest
+import repositories.InMemFlightsRepository
 
 case class Flight(id: String, arrival: Long, departure: Long,
   from: String, to: String, status: String)
@@ -12,21 +13,33 @@ object Protocol {
   implicit val fmtFlight = Json.format[Flight]
 }
 
-class FlightsController(flightRepo: FlightsRepository) extends Controller {
-
+class FlightsController extends Controller {
   import Protocol._
-  def getFlights = Action {
+
+  def addFlight(implicit flightRepo: FlightsRepository) = Action(parse.json) {
+    request =>
+      request.body.validate[Flight] match {
+        case JsSuccess(f, _) =>
+          flightRepo.add(f)
+          Created
+        case JsError(_) => BadRequest
+      }
+  }
+  def getFlights(implicit flightRepo: FlightsRepository) = Action {
     Ok(Json.toJson(flightRepo.loadAll))
   }
+}
 
-  def addFlight = Action(parse.json) { request =>
-    request.body.validate[Flight] match {
-      case JsSuccess(f, _) =>
-        flightRepo.add(f)
-        Created
-      case JsError(_) => BadRequest
-    }
-  }
+object FlightsController extends Controller {
+
+  private lazy val defaultCtlr = new FlightsController
+
+  implicit private lazy val defaultFligthsRepo: FlightsRepository = new InMemFlightsRepository
+
+  def addFlight = defaultCtlr.addFlight
+
+  def getFlights = defaultCtlr.getFlights
+
   // TODO filter by date range
   def getFlightsFrom(cityCode: String) = Action { NotFound }
   def getFlightsTo(cityCode: String) = Action { NotFound }
--- monthly/2014-01/ignasi35/play/january-challenge/test/controllers/FlightsControllerSpec.scala
+++ monthly/2014-01/ignasi35/play/january-challenge/test/controllers/FlightsControllerSpec.scala
@@ -4,10 +4,12 @@ import org.specs2.mutable._
 import play.api.test._
 import play.api.test.Helpers._
 import repositories.FlightsRepository
-import flights.Global
 import repositories.InMemFlightsRepository
 import play.api.libs.json._
 import controllers.Protocol._
+import play.api.mvc.AnyContent
+import play.api.mvc.Action
+import play.api.mvc.Request
 
 trait FlightsFixtures {
   val flightFixture001 = Flight("AA 289", 1393568400000L, 1393576800000L,
@@ -17,33 +19,31 @@ trait FlightsFixtures {
 }
 
 class FlightsControllerSpec extends Specification with FlightsFixtures {
-
-  val globalForTest = new Global {
-    override def flightRepo = new InMemFlightsRepository {
-      override def loadAll(): List[Flight] = List(flightFixture001)
-    }
+  def flightRepo = new InMemFlightsRepository {
+    override def loadAll(): List[Flight] = List(flightFixture001)
   }
 
   "FlightsController" should {
 
     "return 200 on flights/" in {
-      running(FakeApplication(withGlobal = Some(globalForTest))) {
-        val flights = route(FakeRequest(GET, "/flights")).get
-        status(flights) must equalTo(OK)
-        contentType(flights) must beSome.which(_ == "application/json")
-        contentAsString(flights) must equalTo(
-          """[{"id":"AA 289","arrival":1393568400000,"departure":1393576800000,"from":"BOS","to":"CHI","status":"SCHEDULED"}]""")
-      }
+      val flightsAction: Action[AnyContent] = new FlightsController().getFlights(flightRepo)
+      val flights = flightsAction.apply(FakeRequest())
+
+      status(flights) must equalTo(OK)
+      contentType(flights) must beSome.which(_ == "application/json")
+      contentAsString(flights) must equalTo(
+        """[{"id":"AA 289","arrival":1393568400000,"departure":1393576800000,"from":"BOS","to":"CHI","status":"SCHEDULED"}]""")
     }
+
     "return 201 when POSTing a new flight to flights/" in {
-      running(FakeApplication(withGlobal = Some(globalForTest))) {
+      running(FakeApplication()) {
         val body = Json.toJson(flightFixture002)
         val flights = route(FakeRequest(POST, "/flights").withBody(body)).get
         status(flights).toInt must equalTo(201)
       }
     }
     "return 400 when POSTing an invalid JSON to flights/" in {
-      running(FakeApplication(withGlobal = Some(globalForTest))) {
+      running(FakeApplication()) {
         val flights = route(FakeRequest(POST, "/flights").withBody("""{"foo":"bar"}""")).get
         status(flights).toInt must equalTo(400)
       }
