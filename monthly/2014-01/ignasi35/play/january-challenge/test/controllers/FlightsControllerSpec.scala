3ebb1d0286517fca799480ae5fc1c2a179bfcbab
Ignasi Marimon-Clos <ignasi35@gmail.com>
2014-01-26 12:34:40 +0100

    Reviews tests. Improves coverage for controller

--- monthly/2014-01/ignasi35/play/january-challenge/test/controllers/FlightsControllerSpec.scala
+++ monthly/2014-01/ignasi35/play/january-challenge/test/controllers/FlightsControllerSpec.scala
@@ -6,10 +6,14 @@ import play.api.test.Helpers._
 import repositories.FlightsRepository
 import flights.Global
 import repositories.InMemFlightsRepository
+import play.api.libs.json._
+import controllers.Protocol._
 
 trait FlightsFixtures {
   val flightFixture001 = Flight("AA 289", 1393568400000L, 1393576800000L,
     "BOS", "CHI", "SCHEDULED")
+  val flightFixture002 = Flight("VX 925", 1393568400000L, 1393576800000L,
+    "LAX", "SFO", "SCHEDULED")
 }
 
 class FlightsControllerSpec extends Specification with FlightsFixtures {
@@ -29,6 +33,19 @@ class FlightsControllerSpec extends Specification with FlightsFixtures {
         contentType(flights) must beSome.which(_ == "application/json")
         contentAsString(flights) must equalTo(
           """[{"id":"AA 289","arrival":1393568400000,"departure":1393576800000,"from":"BOS","to":"CHI","status":"SCHEDULED"}]""")
+      }
+    }
+    "return 201 when POSTing a new flight to flights/" in {
+      running(FakeApplication(withGlobal = Some(globalForTest))) {
+        val body = Json.toJson(flightFixture002)
+        val flights = route(FakeRequest(POST, "/flights").withBody(body)).get
+        status(flights).toInt must equalTo(201)
+      }
+    }
+    "return 400 when POSTing an invalid JSON to flights/" in {
+      running(FakeApplication(withGlobal = Some(globalForTest))) {
+        val flights = route(FakeRequest(POST, "/flights").withBody("""{"foo":"bar"}""")).get
+        status(flights).toInt must equalTo(400)
       }
     }
 
--- monthly/2014-01/ignasi35/play/january-challenge/test/repositories/FlightsRepositorySpec.scala
+++ monthly/2014-01/ignasi35/play/january-challenge/test/repositories/FlightsRepositorySpec.scala
@@ -6,9 +6,12 @@ import controllers.FlightsFixtures
 class FlightsRepositorySpec extends Specification with FlightsFixtures {
 
   "A FlightsRepository" should {
-    "store fligths I add" in {
+    "store fligths added" in {
       val repo: FlightsRepository = new InMemFlightsRepository
+      repo.loadAll must equalTo(List())
       repo.add(flightFixture001)
+      repo.loadAll must equalTo(List(flightFixture001))
+
     }
   }
 }
