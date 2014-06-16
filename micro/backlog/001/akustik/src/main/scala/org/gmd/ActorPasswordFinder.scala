f7810b7912866d9943f4e50709ae49344ba2c001
Guillem Mercadal <guillem.mercadal@gmail.com>
2014-06-17 00:42:21 +0200

    Almost finished

--- micro/backlog/001/akustik/src/main/scala/org/gmd/ActorPasswordFinder.scala
+++ micro/backlog/001/akustik/src/main/scala/org/gmd/ActorPasswordFinder.scala
@@ -1,49 +1,58 @@
 package org.gmd
 
-import akka.actor.Actor
-import akka.actor.Props
-import akka.actor.ActorSystem
-import java.io.FileInputStream
-import java.io.InputStreamReader
-import java.io.BufferedReader
-import scala.concurrent.duration._
-import scala.language.postfixOps
-import scala.language.implicitConversions
-import akka.actor.ActorLogging
-import javax.xml.bind.DatatypeConverter
-import java.security.MessageDigest
+import akka.actor._
+import java.io._
+import akka.routing._
+import scala.concurrent.duration.Duration
+import java.util.concurrent.TimeUnit
 
 object actors {
-  case class InputFile(path: String)
-  case class Password(pwd: String)
-  case class Query(hash: String)
-  case class Answer(pair: (String, Option[String]))
-  case object DictionaryIsReady
+  
+  type OptionalPassword = (String, Option[String])
+  
+  case class Computation(dictionaryPath: String, hashes: List[String])
+  case class AddPassword(pwd: String)
+  case class QueryForPassword(hashes: List[String])
+  case class PasswordAnswer(answers: List[OptionalPassword])
+  
+  class FacadeActor(pf: ActorPasswordFinder, nHashers: Int = 1) extends Actor {
+    val hashAccActor = context.actorOf(RoundRobinPool(nHashers).props(Props(classOf[HashAccumulatorActor], pf)), "hashers")
+    val lineReaderActor = context.actorOf(Props(classOf[actors.LineReaderActor], hashAccActor), "reader")
+    context.watch(hashAccActor)
+    context.watch(lineReaderActor)
+    var nAnswers = 0
+    var allAnswers: List[OptionalPassword] = Nil
+    var answerTo = self
 
-  class HashAccActor extends Actor {
-    var dict = Map[String, String]()
     def receive = {
-      case Password(null) => {
-        println("Dictionary is loaded!")
-        sender ! DictionaryIsReady
+      case Computation(p, h) => {
+        answerTo = sender
+        lineReaderActor ! Computation(p, h)
       }
-      case Password(pwd) => dict = dict + (computeHash(pwd) -> pwd)
-      case Query(hash) => sender ! Answer(hash, dict get hash)
-    }
-
-    def computeHash(pwd: String): String = {
-      DatatypeConverter.printBase64Binary(MessageDigest.getInstance("SHA-256").digest(pwd.getBytes))
+      case Terminated(ref) => println("terminated: " + ref)
+      case PasswordAnswer(answers) => {
+        nAnswers = nAnswers + 1
+        allAnswers = answers ::: allAnswers
+        if(nHashers == nAnswers) {
+          answerTo ! PasswordAnswer(allAnswers)
+        }
+      }
+      case other => println(this + ": " + other)
     }
   }
 
-  class ReaderActor extends Actor with InOutOps {
-
-    val hashAcc = context.actorOf(Props[HashAccActor], name = "hasher")
-    var accQueries = List[Query]()
-    var accAnswers = List[(String, Option[String])]()
-
+  class HashAccumulatorActor(pf: ActorPasswordFinder) extends Actor {
+    var dict = Map[String, String]()
     def receive = {
-      case InputFile(path) => {
+      case AddPassword(pwd) => dict = dict + (pf.computeHash(pwd) -> pwd)
+      case QueryForPassword(hashes) => sender ! PasswordAnswer(hashes.map(hash => (hash, dict get hash)))
+      case other => println(this + ": " + other)
+    }
+  }
+
+  class LineReaderActor(hashAccActor: ActorRef) extends Actor with InOutOps {
+    def receive = {
+      case Computation(path, hashes) => {
         println(s"reading $path")
         using(new BufferedReader(new InputStreamReader(new FileInputStream(path)))) {
           br =>
@@ -51,40 +60,41 @@ object actors {
               var line: String = null
               do {
                 line = br.readLine()
-                hashAcc ! Password(line)
+                if(line != null) {
+                  hashAccActor ! AddPassword(line)
+                } else {
+                  hashAccActor.tell(Broadcast(QueryForPassword(hashes)), sender)
+                }
               } while (line != null)
             }
         }
       }
-      case Query(hash) => {
-        accQueries = Query(hash) :: accQueries
-      }
-      case DictionaryIsReady => {
-        println("making queries..")
-        accQueries.foreach(hashAcc ! _)
-      }
-      case Answer(pair) => {
-        accAnswers = pair :: accAnswers
-        if(accAnswers.size == accQueries.size) {
-          println(accAnswers)
-          context.system.shutdown
-        }
-      }
+      case other => println(this + ": " + other)
     }
   }
 }
 
 class ActorPasswordFinder extends PasswordFinder {
 
-  override def findMatchesInDictionary(dictionaryPath: String, hashes: List[String]): Set[(String, Option[String])] = {
-    val system = ActorSystem("PasswordFinderSystem")
-    val reader = system.actorOf(Props[actors.ReaderActor], name = "reader")
-    reader ! actors.InputFile(dictionaryPath)
-    hashes foreach(reader ! actors.Query(_))
-    system.awaitTermination(20 seconds)
+  override def findMatchesInDictionary(dictionaryPath: String, hashes: List[String]): Set[(String, Option[String])] = {   
+    val system = ActorSystem("PasswordFinderSystem") 
+    val facadeActor = system.actorOf(Props(classOf[actors.FacadeActor], this, 2))
+    val inbox = Inbox.create(system)
+
+    inbox.send(facadeActor, actors.Computation(dictionaryPath, hashes))
+    inbox.receive(Duration(30, TimeUnit.SECONDS)) match {
+      case actors.PasswordAnswer(answers) => {
+        println(answers)
+        println(answers.groupBy(a => a._1))
+      }
+    }
+    
+    
+    
+    system.shutdown
+    
     Set()
   }
-
 }
 
 object Main {
